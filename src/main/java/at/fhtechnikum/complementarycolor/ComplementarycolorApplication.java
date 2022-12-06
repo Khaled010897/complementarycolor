5ae7b7a6b18114c562e9a158fd97c3348bd00c2b
Khaled Daoud <ic21b019@technikum-wien.at>
2022-12-06 17:59:29 +0100

    Initial commit

---
+++ src/main/java/at/fhtechnikum/complementarycolor/ComplementarycolorApplication.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.complementarycolor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplementarycolorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplementarycolorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtechnikum/complementarycolor/ComplementarycolorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.complementarycolor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplementarycolorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
